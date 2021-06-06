7d9757542abddc7571fbb68e0e0ba0af99598d9d
Taemin Cha <krlrhkstk12@gmail.com>
2021-06-06 20:04:07 +0900

    Version 2: Use additional data structure

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,24 +2,49 @@ import java.io.File
 import java.lang.RuntimeException
 
 fun main() {
-    readFile(".", "")
+    val entry = readFile(".")
+    entry.print("")
 }
 
-fun readFile(path: String, indent: String): Long? {
+sealed interface Entry
+data class SingleFile(val name: String, val size: Long): Entry
+data class Directory(val name: String, val files: List<Entry>): Entry
+
+fun Entry.print(indent: String) {
+    when (this) {
+        is SingleFile -> println("$indent ${this.name} ${this.size}")
+        is Directory -> {
+            val length = this.length()
+            println("$indent ${this.name} $length")
+            this.files.forEach { file -> file.print("$indent    ")}
+        }
+    }
+}
+
+fun Entry.length(): Long {
+    return when (this) {
+        is SingleFile -> this.size
+        is Directory -> {
+            this.files.fold(0L) { acc, entry ->
+                acc + when (entry) {
+                    is SingleFile -> entry.size
+                    is Directory -> entry.length()
+                }
+            }
+        }
+    }
+}
+
+fun readFile(path: String): Entry {
     val entry = File(path)
     if (entry.isDirectory) {
-        val directorySize = entry.listFiles()?.fold(0L) { acc, file ->
-            val fileSize = readFile(file.path, "$indent    ")
-            if (fileSize != null) {
-                return@fold acc + fileSize
-            }
-            return@fold acc
+        val entries = entry.listFiles()?.fold(emptyList<Entry>()) { acc, file ->
+            val subEntry = readFile(file.path)
+            return@fold acc + subEntry
         }
-        println("$indent ${entry.name} $directorySize")
-        return directorySize
+        return Directory(entry.name, entries ?: emptyList())
     } else if (entry.isFile) {
-        println("$indent ${entry.name} ${entry.length()}")
-        return entry.length()
+        return SingleFile(entry.name, entry.length())
     }
     throw RuntimeException("Don't know how to process $entry")
 }
