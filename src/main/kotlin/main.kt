0c8f122bf79f0424095e26e43ca1aa2a0292b4e3
Taemin Cha <krlrhkstk12@gmail.com>
2021-06-06 21:43:54 +0900

    Version 3: Using ForkJoinTask

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,21 @@
 import java.io.File
 import java.lang.RuntimeException
+import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.ForkJoinTask
+import java.util.concurrent.RecursiveTask
+import kotlin.streams.toList
 
 fun main() {
-    val entry = readFile(".")
-    entry.print("")
+    val commonPool = ForkJoinPool.commonPool()
+    val readFileTask = ReadFileTask(File("."))
+    commonPool.invoke(readFileTask)
+    val result = readFileTask.join()
+    result.print("")
 }
 
 sealed interface Entry
 data class SingleFile(val name: String, val size: Long): Entry
-data class Directory(val name: String, val files: List<Entry>): Entry
+data class Directory(val name: String, val size: Long, val entries: List<Entry>): Entry
 
 fun Entry.print(indent: String) {
     when (this) {
@@ -16,7 +23,7 @@ fun Entry.print(indent: String) {
         is Directory -> {
             val length = this.length()
             println("$indent ${this.name} $length")
-            this.files.forEach { file -> file.print("$indent    ")}
+            this.entries.forEach { file -> file.print("$indent    ")}
         }
     }
 }
@@ -25,7 +32,7 @@ fun Entry.length(): Long {
     return when (this) {
         is SingleFile -> this.size
         is Directory -> {
-            this.files.fold(0L) { acc, entry ->
+            this.entries.fold(0L) { acc, entry ->
                 acc + when (entry) {
                     is SingleFile -> entry.size
                     is Directory -> entry.length()
@@ -35,16 +42,28 @@ fun Entry.length(): Long {
     }
 }
 
-fun readFile(path: String): Entry {
-    val entry = File(path)
-    if (entry.isDirectory) {
-        val entries = entry.listFiles()?.fold(emptyList<Entry>()) { acc, file ->
-            val subEntry = readFile(file.path)
-            return@fold acc + subEntry
+class ReadFileTask(private val file: File) : RecursiveTask<Entry>() {
+    override fun compute(): Entry {
+        val subtasks = createSubtasks()
+        if (file.isFile) {
+            return process()
+        } else if (file.isDirectory) {
+            val entries = ForkJoinTask.invokeAll(subtasks).stream().map { task -> task.join() }.toList()
+            return Directory(file.name, entries.sumOf { it.length() }, entries)
         }
-        return Directory(entry.name, entries ?: emptyList())
-    } else if (entry.isFile) {
-        return SingleFile(entry.name, entry.length())
+        throw RuntimeException("Don't know how to handle $file")
     }
-    throw RuntimeException("Don't know how to process $entry")
+
+    private fun createSubtasks(): List<ReadFileTask> =
+        if (file.isDirectory) {
+            file.listFiles()?.fold(emptyList()) { acc, file ->
+                acc + ReadFileTask(file)
+            } ?: emptyList()
+        } else if (file.isFile) {
+            listOf(ReadFileTask(file))
+        } else {
+            emptyList()
+        }
+
+    private fun process(): Entry = SingleFile(file.name, file.length())
 }
